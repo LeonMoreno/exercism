1f4433a82ca66326278f7cdd47daecae0074bcaf
Leonardo Moreno <lmoreno@petalmd.com>
2023-08-25 13:58:34 -0400

    version main update

--- ruby/isogram/isogram.rb
+++ ruby/isogram/isogram.rb
@@ -1,34 +1,13 @@
-# class Isogram
-#   def self.isogram?(input)
-
-#     is_isogram = true
-#     input.downcase!
-#     input.chars.each_index do |index|
-#       if input[index] != '-' && input[index] != ' '
-#         is_isogram = false if input[index + 1, input.length].include?(input[index])
-#       end
-#     end
-#     is_isogram
-#   end
-# end
-
-# if $PROGRAM_NAME == __FILE__
-#   puts Isogram.isogram?("Alphabet")
-#   puts Isogram.isogram?("LeONardo")
-# end
-
-def ejecutar(num)
-  @res1 = num * 2
-  false
+class Isogram
+  def self.isogram?(input)
+    chars = input.downcase.scan(/\w/)
+    chars.each_index do |index|
+      return false if chars[index + 1, chars.length].include?(chars[index])
+    end
+  end
 end
 
-def ensayo(num)
-  
-  ejecutar(num) && true if num % 2 == 0
-  
-
+if $PROGRAM_NAME == __FILE__
+  puts Isogram.isogram?("Alphabet")
+  puts Isogram.isogram?("LeONardo")
 end
-
-res = ensayo(4)
-p res
-p @res1
