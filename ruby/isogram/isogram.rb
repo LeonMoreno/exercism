946e78beefbea60d866726de7d7e3f1dcd38e577
Leonardo Moreno <lmoreno@petalmd.com>
2023-08-25 13:50:23 -0400

    isogram v2: Done

--- ruby/isogram/isogram.rb
+++ ruby/isogram/isogram.rb
@@ -1,34 +1,12 @@
-# class Isogram
-#   def self.isogram?(input)
-
-#     is_isogram = true
-#     input.downcase!
-#     input.chars.each_index do |index|
-#       if input[index] != '-' && input[index] != ' '
-#         is_isogram = false if input[index + 1, input.length].include?(input[index])
-#       end
-#     end
-#     is_isogram
-#   end
-# end
-
-# if $PROGRAM_NAME == __FILE__
-#   puts Isogram.isogram?("Alphabet")
-#   puts Isogram.isogram?("LeONardo")
-# end
-
-def ejecutar(num)
-  @res1 = num * 2
-  false
+class Isogram
+  def self.isogram?(input)
+    text_in_array = input.downcase.scan(/\w/)
+    text_in_array == text_in_array.uniq
+  end
 end
 
-def ensayo(num)
-  
-  ejecutar(num) && true if num % 2 == 0
-  
-
+if $PROGRAM_NAME == __FILE__
+  puts Isogram.isogram?("Alphabet")
+  puts Isogram.isogram?("LeONardo")
+  puts Isogram.isogram?("Leon Mri tquy")
 end
-
-res = ensayo(4)
-p res
-p @res1
